116f5f8940e59ee01e605848715dfc3e0abc7ebf
repingl <repingl@seznam.cz>
2014-02-01 15:39:57 +0100

    second commit

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : Test.cpp
 // Author      : Michal Repka
-// Version     :
+// Version     : 1.0
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
